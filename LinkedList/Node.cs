80ba84ce88d3a2753366c88f05033e5818aa6a80
feredico.gattuso <feredico.gattuso@gmail.com>
2022-04-29 21:10:12 +0300

    Basic node has been added

---
+++ LinkedList/Node.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace LinkedList
+{
+    internal sealed class Node<T> : IEquatable<Node<T>>
+    {
+        private Node<T> next;
+        private T data;
+
+        public Node<T> Next
+        {
+            get => next;
+            set => next = value;
+        }
+        public T Data
+        {
+            get => data;
+            set => data = value != null ? value : throw new ArgumentNullException(nameof(value));
+        }
+
+        public Node() : this(default(T), default(Node<T>)) { }
+        public Node(T data, Node<T> next)
+        {
+            Next = next;
+            Data = data;
+        }
+        public bool Equals([AllowNull] Node<T> other)
+        {
+            if (other != null)
+                return Data.Equals(other.Data);
+            else
+                return false;
+        }
+        public sealed override bool Equals(object obj)
+        {
+            if (obj != null)
+            {
+                if (obj is Node<T> other)
+                {
+                    return Equals(other);
+                }
+            }
+            return false;
+        }
+        public sealed override int GetHashCode() => Data.GetHashCode();
+        public sealed override string ToString() => string.Format("Data: {0}", Data.ToString());
+    }
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node<int> node = new Node<int>(1, null);
+            Node<int> node3 = new Node<int>(1, null);
+            Node<double> node2 = new Node<double>(1, null);
+            Console.WriteLine(node.Equals((object)5));
+            Console.WriteLine(node2);
+            
+        }
+    }
+}
